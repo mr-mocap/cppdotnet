3fca175b423bea6fb00edc36aed640d70fb5ca65
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2024-11-04 18:07:34 -0500

    Add ability to Span over arrays

--- libs/include/System/Span.hpp
+++ libs/include/System/Span.hpp
@@ -1,20 +1,70 @@
 #pragma once
 
 #include <span>
+#include <type_traits>
 
 namespace System
 {
 
-template <class Type>
+template <typename Type, std::size_t Extent = std::dynamic_extent>
 class Span
 {
+    template <typename To, typename From>
+    using is_array_convertible = std::is_convertible<From (*)[], To (*)[]>;
+
+    template <typename Tp, std::size_t ArrayExtent>
+        requires (Extent == std::dynamic_extent || ArrayExtent == Extent)
+	using is_compatible_array = is_array_convertible<Type, Tp>;
 public:
-    constexpr Span() = default;
+      using element_type           = Type;
+      using value_type             = std::remove_cv_t<Type>;
+      using size_type              = std::size_t;
+      using difference_type        = std::ptrdiff_t;
+      using pointer                = Type*;
+      using const_pointer          = const Type*;
+      using reference              = element_type&;
+      using const_reference        = const element_type&;
+      using iterator               = std::span<Type, Extent>::iterator;
+      using reverse_iterator       = std::span<Type, Extent>::reverse_iterator;
+
+    constexpr Span() noexcept
+        requires(Extent == std::dynamic_extent  ||  Extent == 0)
+    {
+    }
+    
+    // Single object span
     explicit constexpr Span(Type &object)
+        requires (Extent == std::dynamic_extent  ||  Extent == 1)
         :
         _data{ &object, 1 }
     {
     }
+
+    // Conversion from built-in arrays
+    template <std::size_t ArrayExtent>
+        requires (Extent == std::dynamic_extent  ||  ArrayExtent == Extent)
+    constexpr Span(std::type_identity_t<Type> (&arr)[ArrayExtent]) noexcept
+        :
+        _data{ arr }
+    {
+    }
+
+    template <typename Tp, std::size_t ArrayExtent>
+        requires is_compatible_array<Tp, ArrayExtent>::value
+    constexpr Span(std::array<Tp, ArrayExtent> &arr) noexcept
+        :
+        _data{ arr }
+    {
+    }
+
+    template <typename Tp, std::size_t ArrayExtent>
+        requires is_compatible_array<const Tp, ArrayExtent>::value
+    constexpr Span(const std::array<Tp, ArrayExtent> &arr) noexcept
+        :
+        _data{ arr }
+    {
+    }
+
     Span(Type &&object) = delete;
 
 
@@ -36,5 +86,18 @@ public:
 protected:
     std::span<Type> _data;
 };
+
+// Deduction guides
+template<typename Type>
+Span(Type &) -> Span<Type, 1>;
+
+template<typename Type, size_t Extent>
+Span(Type (&)[Extent]) -> Span<Type, Extent>;
+
+template<typename Type, size_t Extent>
+Span(std::array<Type, Extent> &) -> Span<Type, Extent>;
+
+template<typename Type, size_t Extent>
+Span(const std::array<Type, Extent> &) -> Span<const Type, Extent>;
 
 }
--- testing/TestSpan.cpp
+++ testing/TestSpan.cpp
@@ -34,6 +34,68 @@ void SingleObject()
     assert( one_item[0] == var );
 }
 
+void MultipleObjectsFromBuiltInArray()
+{
+    std::cout << __func__ << std::endl;
+
+    int builtin_array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    System::Span<int> span_of_items( builtin_array );
+
+    assert( span_of_items.Length() == 10 );
+    assert( span_of_items[0] == 1 );
+    assert( span_of_items[1] == 2 );
+    assert( span_of_items[2] == 3 );
+    assert( span_of_items[3] == 4 );
+    assert( span_of_items[4] == 5 );
+    assert( span_of_items[5] == 6 );
+    assert( span_of_items[6] == 7 );
+    assert( span_of_items[7] == 8 );
+    assert( span_of_items[8] == 9 );
+    assert( span_of_items[9] == 10 );
+
+    span_of_items[3] = 99;
+
+    assert( span_of_items[3] == 99 );
+}
+
+void MultipleObjectsFromStdArray()
+{
+    std::cout << __func__ << std::endl;
+
+    // Non-const
+    {
+        std::array<int, 6> a{ 1, 2, 3, 4, 5, 6 };
+        System::Span<int> span_a( a );
+
+        assert( span_a.Length() == 6 );
+    }
+
+    // Const std::array to Span of const items
+    {
+        const std::array<int, 3> a{ 1, 2, 3 };
+        System::Span<const int> span_a( a );
+
+        assert( span_a.Length() == 3 );
+    }
+#if 0
+    // std::array of const items to const Span of items
+    {
+        std::array<const int, 3> a{ 1, 2, 3 };
+        const System::Span<int> span_a( a );
+
+        assert( span_a.Length() == 3 );
+    }
+#endif
+
+    // std::array of const items to Span of const items
+    {
+        std::array<const int, 3> a{ 1, 2, 3 };
+        System::Span<const int, 3> span_a( a );
+
+        assert( span_a.Length() == 3 );
+    }
+}
+
 void Assignment()
 {
     std::cout << __func__ << std::endl;
@@ -65,6 +127,7 @@ void Run()
     Empty();
     DefaultConstructedSpanIsEmpty();
     SingleObject();
+    MultipleObjectsFromBuiltInArray();
     Assignment();
 
     std::cout << "PASSED!" << std::endl;
