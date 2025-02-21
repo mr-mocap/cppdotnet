25c8d619a20179190a92291efab70906762ab288
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-02-21 10:35:41 -0500

    Add a few more Console related structs

---
+++ libs/include/System/ConsoleColor.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+
+namespace System
+{
+
+enum class ConsoleColor
+{
+    Black,
+    DarkBlue,
+    DarkGreen,
+    DarkCyan,
+    DarkRed,
+    DarkMagenta,
+    DarkYellow,
+    Gray,
+    DarkGray,
+    Blue,
+    Green,
+    Cyan,
+    Red,
+    Magenta,
+    Yellow,
+    White
+};
+
+}
---
+++ libs/include/System/ConsoleModifiers.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+
+namespace System
+{
+
+enum class consoleModifiers
+{
+    None    = 0,
+    Alt     = 1, ///@< The left or right ALT modifier key
+    Shift   = 2, ///@< The left or right SHIFT modifier key
+    Control = 4  ///@< The left or right CTRL modifier key
+};
+
+}
