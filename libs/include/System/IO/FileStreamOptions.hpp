fe3a0f094a8617eb2b73bcfaa8f5d8fb72c4c0f4
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-02-24 12:10:23 -0500

    Slightly improve FileStreamOptions class

--- libs/include/System/IO/FileStreamOptions.hpp
+++ libs/include/System/IO/FileStreamOptions.hpp
@@ -2,6 +2,8 @@
 
 #include "System/IO/FileAccess.hpp"
 #include "System/IO/FileMode.hpp"
+#include "System/Nullable.hpp"
+#include "System/IO/UnixFileMode.hpp"
 
 
 namespace System::IO
@@ -11,22 +13,36 @@ class FileStreamOptions
 {
 public:
 
-    const FileAccess &Access() const { return _access; }
+    FileAccess Access() const { return _access; }
 
-    size_t BufferSize() const { return _buffer_size; }
+    size_t     BufferSize() const { return _buffer_size; }
 
-    const FileMode &Mode() const { return _mode; }
+    FileMode   Mode() const { return _mode; }
 
+    Nullable<UnixFileMode> UnixCreateMode() const { return _unix_file_mode; }
+    void UnixCreateMode(Nullable<UnixFileMode> ufm)
+    {
+        _unix_file_mode = ufm;
+    }
 #if 0
     const FileOptions &Options() const { return _options; }
 #endif
+
+    bool operator ==(const FileStreamOptions &other) const
+    {
+        return (_access == other._access) &&
+               (_mode == other._mode) &&
+               (_unix_file_mode == other._unix_file_mode);
+    }
 protected:
-    FileAccess _access;
-    FileMode   _mode;
+    size_t     _buffer_size = 0;
+    FileAccess _access = FileAccess::ReadWrite;
+    FileMode   _mode   = FileMode::Open;
+    Nullable<UnixFileMode> _unix_file_mode;
 #if 0
     FileOptions _options;
 #endif
-    size_t     _buffer_size;
+
 };
 
 }
