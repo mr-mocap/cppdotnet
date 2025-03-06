cd365bf2b65b4881b721bf2362bf1de79484e4b7
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-03-06 10:55:01 -0500

    Fix compilation error (typo)

--- libs/include/System/Collections/Generic/ICollection.hpp
+++ libs/include/System/Collections/Generic/ICollection.hpp
@@ -16,7 +16,7 @@ class ICollection
         virtual std::size_t Count() const = 0;
         virtual bool        IsReadOnly() const = 0;
 
-        virtual void Add(const T &item = 0;
+        virtual void Add(const T &item) = 0;
         virtual bool Remove(const T &item) = 0;
         virtual void Clear() = 0;
         virtual bool Contains(const T &item) = 0;
