446af79ff2d578b470287b8597e9a0b1f593f37e
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-03-27 14:02:27 -0400

    Initial idea for use KeyValuePair as part of ICollection for a Dictionary

--- libs/include/System/Collections/Generic/KeyValuePair.hpp
+++ libs/include/System/Collections/Generic/KeyValuePair.hpp
@@ -8,12 +8,11 @@ namespace System::Collections::Generic
 {
 
 template <class TKey, class TValue>
-struct KeyValuePair
+struct KeyValuePair : std::pair<const TKey, TValue>
 {
     KeyValuePair(const TKey &k, const TValue &v)
         :
-        Key{ k },
-        Value{ v }
+        std::pair( k, v )
     {
     }
 
@@ -23,8 +22,17 @@ struct KeyValuePair
         return std::format("[{}, {}]", Key, Value);
     }
 
-    TKey   Key;
-    TValue Value;
+    const TKey &Key() const { return first; }
+    const TKey &Key()       { return first; }
+
+    const TValue &Value() const { return second; }
+          TValue &Value()       { return second; }
+    
+    void Deconstruct(TKey &key_out, TValue &value_out)
+    {
+        key_out = Key();
+        value_out = Value();
+    }
 };
 
 }
