0af57db1bac45c8722988272965ba58477901dc4
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-06-02 10:32:47 -0400

    Add more enum formatting to Diagnostics & IO

--- libs/include/System/Diagnostics/Activity.hpp
+++ libs/include/System/Diagnostics/Activity.hpp
@@ -272,3 +272,321 @@ protected:
 };
 
 }
+
+namespace System
+{
+
+template <>
+struct EnumTraits<Diagnostics::ActivityTraceFlags> : EnumTraitTypes<Diagnostics::ActivityTraceFlags>
+{
+    static auto EnumName() -> std::string_view { return "ActivityTraceFlags"; }
+
+    static constexpr name_value_pair_type NameValueArray[] = {
+            { "None",     Diagnostics::ActivityTraceFlags::None },
+            { "Recorded", Diagnostics::ActivityTraceFlags::Recorded }
+        };
+
+    static constexpr auto NameValuePairs() -> std::span<const name_value_pair_type>
+    {
+        return std::span{ NameValueArray };
+    }
+
+    static constexpr auto Count() -> std::size_t { return NameValuePairs().size(); }
+
+    static constexpr auto Names() -> std::ranges::keys_view<std::span<const name_value_pair_type>>
+    {
+        return std::views::keys( NameValuePairs() );
+    }
+
+    static constexpr auto Values() -> std::ranges::values_view<std::span<const name_value_pair_type>>
+    {
+        return std::views::values( NameValuePairs() );
+    }
+
+    static constexpr auto ValuesAsUnderlyingType()
+    {
+        auto to_underlying_type = [](const value_type item) { return static_cast<underlying_type>(item); };
+
+        return Values() | std::views::transform( to_underlying_type );
+    }
+
+    static constexpr bool IsDefined(value_type value)
+    {
+        return std::ranges::find( NameValuePairs(),
+                                  value,
+                                  &name_value_pair_type::second
+                                ) != NameValuePairs().end();
+    }
+
+    static constexpr bool IsDefined(underlying_type value)
+    {
+        return IsDefined( static_cast<value_type>(value) );
+    }
+
+    static constexpr bool IsDefined(std::string_view value_string)
+    {
+        return std::ranges::find( NameValuePairs(),
+                                  value_string,
+                                  &name_value_pair_type::first
+                                ) != NameValuePairs().end();
+    }
+
+    static constexpr auto ToName(value_type value) -> std::string_view
+    {
+        auto found = std::ranges::find( NameValuePairs(),
+                                        value,
+                                        &name_value_pair_type::second
+                                      );
+
+        if ( found == NameValuePairs().end() )
+            return {};
+
+        return found->first;
+    }
+
+    static constexpr auto ToName(underlying_type value) -> std::string_view
+    {
+        return ToName( static_cast<value_type>(value) );
+    }
+
+    static constexpr value_type min() { return Diagnostics::ActivityTraceFlags::None; }
+    static constexpr value_type max() { return Diagnostics::ActivityTraceFlags::Recorded; }
+};
+
+template <>
+struct EnumTraits<Diagnostics::ActivityStatusCode> : EnumTraitTypes<Diagnostics::ActivityStatusCode>
+{
+    static auto EnumName() -> std::string_view { return "ActivityStatusCode"; }
+
+    static constexpr name_value_pair_type NameValueArray[] = {
+            { "Unset", Diagnostics::ActivityStatusCode::Unset },
+            { "Ok",    Diagnostics::ActivityStatusCode::Ok },
+            { "Error", Diagnostics::ActivityStatusCode::Error }
+        };
+
+    static constexpr auto NameValuePairs() -> std::span<const name_value_pair_type>
+    {
+        return std::span{ NameValueArray };
+    }
+
+    static constexpr auto Count() -> std::size_t { return NameValuePairs().size(); }
+
+    static constexpr auto Names() -> std::ranges::keys_view<std::span<const name_value_pair_type>>
+    {
+        return std::views::keys( NameValuePairs() );
+    }
+
+    static constexpr auto Values() -> std::ranges::values_view<std::span<const name_value_pair_type>>
+    {
+        return std::views::values( NameValuePairs() );
+    }
+
+    static constexpr auto ValuesAsUnderlyingType()
+    {
+        auto to_underlying_type = [](const value_type item) { return static_cast<underlying_type>(item); };
+
+        return Values() | std::views::transform( to_underlying_type );
+    }
+
+    static constexpr bool IsDefined(value_type value)
+    {
+        return std::ranges::find( NameValuePairs(),
+                                  value,
+                                  &name_value_pair_type::second
+                                ) != NameValuePairs().end();
+    }
+
+    static constexpr bool IsDefined(underlying_type value)
+    {
+        return IsDefined( static_cast<value_type>(value) );
+    }
+
+    static constexpr bool IsDefined(std::string_view value_string)
+    {
+        return std::ranges::find( NameValuePairs(),
+                                  value_string,
+                                  &name_value_pair_type::first
+                                ) != NameValuePairs().end();
+    }
+
+    static constexpr auto ToName(value_type value) -> std::string_view
+    {
+        auto found = std::ranges::find( NameValuePairs(),
+                                        value,
+                                        &name_value_pair_type::second
+                                      );
+
+        if ( found == NameValuePairs().end() )
+            return {};
+
+        return found->first;
+    }
+
+    static constexpr auto ToName(underlying_type value) -> std::string_view
+    {
+        return ToName( static_cast<value_type>(value) );
+    }
+
+    static constexpr value_type min() { return Diagnostics::ActivityStatusCode::Unset; }
+    static constexpr value_type max() { return Diagnostics::ActivityStatusCode::Error; }
+};
+
+template <>
+struct EnumTraits<Diagnostics::ActivityKind> : EnumTraitTypes<Diagnostics::ActivityKind>
+{
+    static auto EnumName() -> std::string_view { return "ActivityKind"; }
+
+    static constexpr name_value_pair_type NameValueArray[] = {
+            { "Internal", Diagnostics::ActivityKind::Internal },
+            { "Server",   Diagnostics::ActivityKind::Server },
+            { "Client",   Diagnostics::ActivityKind::Client },
+            { "Producer", Diagnostics::ActivityKind::Producer },
+            { "Consumer", Diagnostics::ActivityKind::Consumer }
+        };
+
+    static constexpr auto NameValuePairs() -> std::span<const name_value_pair_type>
+    {
+        return std::span{ NameValueArray };
+    }
+
+    static constexpr auto Count() -> std::size_t { return NameValuePairs().size(); }
+
+    static constexpr auto Names() -> std::ranges::keys_view<std::span<const name_value_pair_type>>
+    {
+        return std::views::keys( NameValuePairs() );
+    }
+
+    static constexpr auto Values() -> std::ranges::values_view<std::span<const name_value_pair_type>>
+    {
+        return std::views::values( NameValuePairs() );
+    }
+
+    static constexpr auto ValuesAsUnderlyingType()
+    {
+        auto to_underlying_type = [](const value_type item) { return static_cast<underlying_type>(item); };
+
+        return Values() | std::views::transform( to_underlying_type );
+    }
+
+    static constexpr bool IsDefined(value_type value)
+    {
+        return std::ranges::find( NameValuePairs(),
+                                  value,
+                                  &name_value_pair_type::second
+                                ) != NameValuePairs().end();
+    }
+
+    static constexpr bool IsDefined(underlying_type value)
+    {
+        return IsDefined( static_cast<value_type>(value) );
+    }
+
+    static constexpr bool IsDefined(std::string_view value_string)
+    {
+        return std::ranges::find( NameValuePairs(),
+                                  value_string,
+                                  &name_value_pair_type::first
+                                ) != NameValuePairs().end();
+    }
+
+    static constexpr auto ToName(value_type value) -> std::string_view
+    {
+        auto found = std::ranges::find( NameValuePairs(),
+                                        value,
+                                        &name_value_pair_type::second
+                                      );
+
+        if ( found == NameValuePairs().end() )
+            return {};
+
+        return found->first;
+    }
+
+    static constexpr auto ToName(underlying_type value) -> std::string_view
+    {
+        return ToName( static_cast<value_type>(value) );
+    }
+
+    static constexpr value_type min() { return Diagnostics::ActivityKind::Internal; }
+    static constexpr value_type max() { return Diagnostics::ActivityKind::Consumer; }
+};
+
+template <>
+struct EnumTraits<Diagnostics::ActivityIdFormat> : EnumTraitTypes<Diagnostics::ActivityIdFormat>
+{
+    static auto EnumName() -> std::string_view { return "ActivityIdFormat"; }
+
+    static constexpr name_value_pair_type NameValueArray[] = {
+            { "Unknown",      Diagnostics::ActivityIdFormat::Unknown },
+            { "Hierarchical", Diagnostics::ActivityIdFormat::Hierarchical },
+            { "W3C",          Diagnostics::ActivityIdFormat::W3C }
+        };
+
+    static constexpr auto NameValuePairs() -> std::span<const name_value_pair_type>
+    {
+        return std::span{ NameValueArray };
+    }
+
+    static constexpr auto Count() -> std::size_t { return NameValuePairs().size(); }
+
+    static constexpr auto Names() -> std::ranges::keys_view<std::span<const name_value_pair_type>>
+    {
+        return std::views::keys( NameValuePairs() );
+    }
+
+    static constexpr auto Values() -> std::ranges::values_view<std::span<const name_value_pair_type>>
+    {
+        return std::views::values( NameValuePairs() );
+    }
+
+    static constexpr auto ValuesAsUnderlyingType()
+    {
+        auto to_underlying_type = [](const value_type item) { return static_cast<underlying_type>(item); };
+
+        return Values() | std::views::transform( to_underlying_type );
+    }
+
+    static constexpr bool IsDefined(value_type value)
+    {
+        return std::ranges::find( NameValuePairs(),
+                                  value,
+                                  &name_value_pair_type::second
+                                ) != NameValuePairs().end();
+    }
+
+    static constexpr bool IsDefined(underlying_type value)
+    {
+        return IsDefined( static_cast<value_type>(value) );
+    }
+
+    static constexpr bool IsDefined(std::string_view value_string)
+    {
+        return std::ranges::find( NameValuePairs(),
+                                  value_string,
+                                  &name_value_pair_type::first
+                                ) != NameValuePairs().end();
+    }
+
+    static constexpr auto ToName(value_type value) -> std::string_view
+    {
+        auto found = std::ranges::find( NameValuePairs(),
+                                        value,
+                                        &name_value_pair_type::second
+                                      );
+
+        if ( found == NameValuePairs().end() )
+            return {};
+
+        return found->first;
+    }
+
+    static constexpr auto ToName(underlying_type value) -> std::string_view
+    {
+        return ToName( static_cast<value_type>(value) );
+    }
+
+    static constexpr value_type min() { return Diagnostics::ActivityIdFormat::Unknown; }
+    static constexpr value_type max() { return Diagnostics::ActivityIdFormat::W3C; }
+};
+
+}
