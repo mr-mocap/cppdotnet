85e56ad0493061820a3b20e26a61ae2cd3083ce5
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-06-23 10:27:33 -0400

    Bugfix: Include KeyNotFoundException class in compilation

--- libs/src/System/Collections/Generic/KeyNotFoundException.cpp
+++ libs/src/System/Collections/Generic/KeyNotFoundException.cpp
@@ -6,9 +6,7 @@ namespace System::Collections::Generic
 
 std::string_view KeyNotFoundException::ClassName() const
 {
-    using namespace std::literals;
-
-    return "KeyNotFoundException"sv;
+    return "KeyNotFoundException";
 }
 
 }
