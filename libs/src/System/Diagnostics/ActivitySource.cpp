09f449ffe2ff90fefe377d1febc11d4c256a04b6
David Harrison <16119203+mr-mocap@users.noreply.github.com>
2025-06-25 18:22:23 -0400

    Quiet compile warnings

--- libs/src/System/Diagnostics/ActivitySource.cpp
+++ libs/src/System/Diagnostics/ActivitySource.cpp
@@ -1,4 +1,5 @@
 #include "System/Diagnostics/ActivitySource.hpp"
+#include "System/Private/private.hpp"
 
 
 namespace System::Diagnostics
@@ -9,11 +10,16 @@ Activity ActivitySource::CreateActivity(std::string_view  name,
                                         ActivityContext  &parent_context,
                                         ActivityIdFormat  id_format)
 {
+    UNUSED(kind);
+    UNUSED(parent_context);
+    UNUSED(id_format);
+
     return Activity(name);
 }
 
 void ActivitySource::AddActivityListener(const ActivityListener &listener)
 {
+    UNUSED(listener);
 }
 
 bool ActivitySource::HasListeners() const
