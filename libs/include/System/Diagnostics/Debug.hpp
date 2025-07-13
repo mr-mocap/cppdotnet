78c125742c4b1e79114623e3d549944231ca7b31
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-07-13 19:25:46 -0400

    Improve usage of format_string

--- libs/include/System/Diagnostics/Debug.hpp
+++ libs/include/System/Diagnostics/Debug.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "System/Diagnostics/TraceListenerCollection.hpp"
+#include "System/Diagnostics/Private/Formatting.hpp"
 #include <string>
+#include <string_view>
 #include <format>
 #include <source_location>
+#include <concepts>
 
 namespace System::Diagnostics
 {
@@ -29,67 +32,76 @@ public:
     static void Write(std::string_view message);
     static void Write(std::string_view message, std::string_view category);
 
-    template <typename ...Args>
-    static void Write(std::format_string<Args...> &&fmt, Args &&... args)
+    template <typename T, typename ...Args>
+        requires (!std::convertible_to<T, std::string_view> || sizeof...(Args) > 0)
+    static void Write(std::format_string<T, Args...> &&fmt, T &&arg1, Args &&... args)
     {
-        Write( std::string_view( std::vformat( fmt.get(), std::make_format_args( args... ) ) ) );
+        Write( std::string_view( std::vformat( fmt.get(), std::make_format_args( arg1, args... ) ) ) );
     }
 
     static void WriteIf(bool condition, std::string_view message);
     static void WriteIf(bool condition, std::string_view message, std::string_view category);
 
-    template <typename ...Args>
-    static void WriteIf(bool condition, std::format_string<Args...> &&fmt, Args &&... args)
+    template <typename T, typename ...Args>
+        requires (!std::convertible_to<T, std::string_view> || sizeof...(Args) > 0)
+    static void WriteIf(bool condition, std::format_string<T, Args...> &&fmt, T &&arg1, Args &&... args)
     {
-        WriteIf( condition, std::string_view( std::vformat( fmt.get(), std::make_format_args( args... ) ) ) );
+        WriteIf( condition, std::string_view( std::vformat( fmt.get(), std::make_format_args( arg1, args... ) ) ) );
     }
 
     static void WriteLine(std::string_view message);
     static void WriteLine(std::string_view message, std::string_view category);
 
-    template <typename ...Args>
-    static void WriteLine(std::format_string<Args...> &&fmt, Args &&... args)
+    template <typename T, typename ...Args>
+        requires (!std::convertible_to<T, std::string_view> || sizeof...(Args) > 0)
+    static void WriteLine(std::format_string<T, Args...> &&fmt, T &&arg1, Args &&... args)
     {
-        WriteLine( std::string_view( std::vformat( fmt.get(), std::make_format_args( args... ) ) ) );
+        WriteLine( std::string_view( std::vformat( fmt.get(), std::make_format_args( arg1, args... ) ) ) );
     }
 
     static void WriteLineIf(bool condition, std::string_view message);
     static void WriteLineIf(bool condition, std::string_view message, std::string_view category);
 
-    template <typename ...Args>
-    static void WriteLineIf(bool condition, std::format_string<Args...> &&fmt, Args &&... args)
+    template <typename T, typename ...Args>
+        requires (!std::convertible_to<T, std::string_view> || sizeof...(Args) > 0)
+    static void WriteLineIf(bool condition, std::format_string<T, Args...> &&fmt, T &&arg1, Args &&... args)
     {
-        WriteLineIf( condition, std::string_view( std::vformat( fmt.get(), std::make_format_args( args... ) ) ) );
+        WriteLineIf( condition, std::string_view( std::vformat( fmt.get(), std::make_format_args( arg1, args... ) ) ) );
     }
 
     static void Print(std::string_view message);
 
-    template <typename ...Args>
-    static void Print(std::format_string<Args...> &&fmt, Args &&... args)
+    template <typename T, typename ...Args>
+        requires (!std::convertible_to<T, std::string_view> || sizeof...(Args) > 0)
+    static void Print(std::format_string<T, Args...> &&fmt, T &&arg1, Args &&... args)
     {
-        Print( std::string_view( std::vformat( fmt.get(), std::make_format_args( args... ) ) ) );
+        Print( std::string_view( std::vformat( fmt.get(), std::make_format_args( arg1, args... ) ) ) );
     }
 
     static void Assert(bool condition, const std::source_location = std::source_location::current());
-    static void Assert(bool condition, std::string_view message,
-                       const std::source_location = std::source_location::current());
-    static void Assert(bool condition, std::string_view message, std::string_view category,
-                       const std::source_location = std::source_location::current());
-#if 0
-    template <typename ...Args>
-    static void Assert(bool condition, std::format_string<Args...> &&fmt, Args &&... args)
-    {
-        Assert( condition, std::string_view( std::vformat( fmt.get(), std::make_format_args( args... ) ) ), std::source_location::current() );
-    }
-#endif
+    static void Assert(bool condition, std::string_view  message, const std::source_location = std::source_location::current());
+    static void Assert(bool condition, std::string_view  message, std::string_view  category, const std::source_location = std::source_location::current());
+    // static void Assert(bool condition, const FormatCall &call, const std::source_location loc = std::source_location::current())
+    // {
+    //     Assert( condition, std::string_view(call), loc);
+    // }
+    // template <typename ...Args>
+    //     requires (sizeof...(Args) > 0)
+    // static void Assert(FormatTag, bool condition, std::format_string<Args...> &&fmt, Args &&... args)
+    // {
+    //     // TODO: Implement this properly (support std::source_location)
+    //     Assert( condition, std::string_view( std::vformat( fmt.get(), std::make_format_args( args... ) ) ) );
+    // }
+
 
     static void Fail(std::string_view message);
     static void Fail(std::string_view message, std::string_view category);
 
-    template <typename ...Args>
-    static void Fail(std::format_string<Args...> &&fmt, Args &&... args)
+    template <typename T, typename ...Args>
+        requires (!std::convertible_to<T, std::string_view> || sizeof...(Args) > 0)
+    static void Fail(std::format_string<T, Args...> &&fmt, T &&arg1, Args &&... args)
     {
-        Fail( std::string_view( std::vformat( fmt.get(), std::make_format_args( args... ) ) ) );
+        Fail( std::string_view( std::vformat( fmt.get(), std::make_format_args( arg1, args... ) ) ) );
     }
 
     static TraceListenerCollection &Listeners();
--- testing/TestDebug.cpp
+++ testing/TestDebug.cpp
@@ -29,13 +29,31 @@ void CommonUsage()
 
     Debug::Assert(false);
     Debug::Assert(false, "Assert with message");
-    Debug::Assert(false, "Assert with parameter in message: '{}'", "std::string");
+    Debug::Assert(false, "Assert with message and category", "Test.Category");
+    Debug::Assert(false, System::Diagnostics::FormatCall("Assert with parameter in message: '{}'", "std::string") );
+    Debug::Assert(true, System::Diagnostics::FormatCall("Assert with parameter in message: '{}'", "std::string") );
 
     Debug::Fail("Debug::Fail()"sv);
-    Debug::Fail("Debug::Fail() #{}", 42);
+    Debug::Fail("Debug::Fail()", "Test.Category");
+    Debug::Fail("Debug::Fail()", std::string("Test.Category"));
+    Debug::Fail("Debug::Fail() with parameter #{}", 42);
+    Debug::Fail("Debug::Fail() with parameter #{} {}", 42, "param");
 
-    Debug::WriteLineIf(true, "Debug::WriteLineIf(true, ...)"sv);
-    Debug::WriteLineIf(false, "Debug::WriteLineIf(false, ...)"sv);
+    // This will be interpreted as a message and category (cannot resolve the overload)
+    Debug::Fail("Debug::Fail() with parameter #{}", "param");
+
+    // But we can get around it by using FormatCall
+    Debug::Fail( System::Diagnostics::FormatCall("Debug::Fail() with parameter #{}", "param") );
+
+    // Or by having more than one parameter
+    Debug::Fail("Debug::Fail() with parameter {} ${}", "param", 16 );
+
+    Debug::WriteLineIf(true, "Debug::WriteLineIf(true, ...)");
+    Debug::WriteLineIf(false, "Debug::WriteLineIf(false, ...)");
+    Debug::WriteLineIf(true, "Debug::WriteLineIf(true, ...)", "Debug.Category");
+
+    // This will be interpreted as a message and category (cannot resolve the overload)
+    Debug::WriteLineIf(true, "Debug::WriteLineIf(true, ...) With Parameter '{}'", "Example Parameter");
 
     Debug::Listeners().Remove(consoleListener.get());
 }
