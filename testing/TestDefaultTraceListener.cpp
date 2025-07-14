228835f2fd28a82c4cafb5906e74dfd7f0f16638
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-07-14 18:20:08 -0400

    Slightly improve unit test

--- testing/TestDefaultTraceListener.cpp
+++ testing/TestDefaultTraceListener.cpp
@@ -7,22 +7,25 @@
 namespace TestDefaultTraceListener
 {
 
-void CommonUsage()
+void AccessingListenersFirstTimeCreatesDefaultTraceListener()
 {
     std::cout << __func__ << std::endl;
 
-    assert( System::Diagnostics::Debug::Listeners().Count() == 1 );
+    assert( System::Diagnostics::Trace::Listeners().Count() == 1 );
+    assert( System::Diagnostics::Trace::Listeners()["Default"] );
 
-    System::Diagnostics::Debug::WriteLine("This is a test!");
-    System::Diagnostics::Debug::WriteLineIf(true, "This is the value {}", 42);
-    System::Diagnostics::Debug::WriteLineIf(false, "This is the value {}", 42);
+    System::Diagnostics::Trace::WriteLine("This is a test!");
+    System::Diagnostics::Trace::WriteLineIf(true, "This is the value {}", 42);
+    
+    // Doesn't output anything
+    System::Diagnostics::Trace::WriteLineIf(false, "This is the value {}", 42);
 }
 
 void Run()
 {
     std::cout << "Running DefaultTraceListener Tests..." << std::endl;
 
-    CommonUsage();
+    AccessingListenersFirstTimeCreatesDefaultTraceListener();
 
     std::cout << "PASSED!" << std::endl;
 }
