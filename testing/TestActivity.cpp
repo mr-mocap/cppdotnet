3accba3289bd814dd14bb164f47d3b30c4fa2867
David Harrison <16119203+mr-mocap@users.noreply.github.com>
2025-07-28 22:34:21 -0400

    Unfinished work

---
+++ testing/TestActivity.cpp
@@ -0,0 +1,32 @@
+#include "TestActivity.hpp"
+#include "System/Console.hpp"
+#include "System/IO/TextWriter.hpp"
+#include "System/Diagnostics/ActivitySource.hpp"
+
+namespace TestActivity
+{
+
+void BasicActivity()
+{
+    System::Console::Out().WriteLine(__func__);
+
+    System::Diagnostics::ActivitySource source("Example Activity Source");
+    std::optional<System::Diagnostics::Activity> activity = source.StartActivity("Example Operation");
+
+    if ( activity )
+    {
+
+    }
+}
+
+void Run()
+{
+    System::Console::Out().WriteLine("Running Activity Tests...");
+
+    BasicActivity();
+
+    System::Console::Out().WriteLine("PASSED!");
+
+}
+
+}
