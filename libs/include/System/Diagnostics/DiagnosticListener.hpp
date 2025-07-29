55ea3c868ab7c8d7cfd2ae1e8a3a6d7c24fc84ee
David Harrison <16119203+mr-mocap@users.noreply.github.com>
2025-07-28 22:33:42 -0400

    Unfinished work

--- libs/include/System/Diagnostics/DiagnosticListener.hpp
+++ libs/include/System/Diagnostics/DiagnosticListener.hpp
@@ -1,23 +1,37 @@
 #pragma once
 
 #include "System/Diagnostics/DiagnosticSource.hpp"
+#include "System/IDisposable.hpp"
+#include "System/IObservable.hpp"
+#include "System/IObserver.hpp"
+#include "System/Collections/Generic/KeyValuePair.hpp"
+#include "System/Collections/Generic/List.hpp"
 #include <string>
 
 
 namespace System::Diagnostics
 {
 
-class DiagnosticListener : public DiagnosticSource
+class DiagnosticListener : public DiagnosticSource,
+                           public IObservable<Collections::Generic::KeyValuePair<std::string, void *>>
 {
 public:
     DiagnosticListener(std::string_view name);
 
     const std::string &Name() const { return _name; }
 
+    bool IsEnabled() const override;
+    bool IsEnabled(std::string_view name) const override;
+
     // IObservable<T> interface
-    Subscribe();
+    // AllListeners();
+
+    virtual IDisposable Subscribe(IObserver<Collections::Generic::KeyValuePair<std::string, void *>> &observer) override;
+
+    void Dispose();
 protected:
     std::string _name;
+    Collections::Generic::List<Collections::Generic::KeyValuePair<std::string, void *>> _observers;
 };
 
 }
