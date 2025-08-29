0279b387edb64ca7006fa315854dd5cf8eb69d00
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-08-29 11:03:49 -0400

    Improvement in recognition of imports

--- libs/list_dependencies.sh
+++ libs/list_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for file in "$@"; do
-  grep -E '^(import|#include)[[:space:]]*["<][^">]+[">]' "$file" | while read -r line; do
-    kind=$(echo "$line" | grep -oE '^(import|#include)')
-    path=$(echo "$line" | grep -oE '["<][^">]+[">]' | tr -d '<>"')
+  grep -E '^(import|#include|export import)[[:space:]]*(["<][^">]+[">]|[a-zA-Z:_]+)' "$file" | while read -r line; do
+    kind=$(echo "$line" | grep -oE '^(import|#include|export import)')
+    path=$(echo "$line" | grep -oP '(?<=[[:space:]])(["<][^">]+[">]|[a-zA-Z:_]+)(?=;)' | tr -d '<>"')
     type=$(echo "$line" | grep -qE '<' && echo "system" || echo "user")
     echo -e "${file}\t${kind}\t${type}\t${path}"
   done
